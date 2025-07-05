1499cf7daeb3787bfb058907389a72c5c3f3806f
Chetan Pawar <chetanpawar2355259@gmail.com>
2025-07-05 17:07:00 +0530

    OOP in Java

---
+++ Day 10/Oops.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+class GetInfo{
+    void string(String name){
+        System.out.println(name);
+    }
+    void integer(int num){
+        System.out.println(num);
+    }
+}
+public class Oops {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the Name: ");
+        String str = sc.nextLine();
+        System.out.print("Enter the Number: ");
+        int num = sc.nextInt();
+        GetInfo s = new GetInfo();
+        s.integer(num);
+        s.string(str);
+    }
+}
